d53bcccb060315917310478d712748ec8e441946
Oleh Dibrov <Oleh_Dibrov@epam.com>
2019-07-08 17:25:47 +0300

    Changes in TestClass

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -3,4 +3,8 @@ public class TestClass {
     public void testingMethod() {
         System.out.println("method for testing");
     }
+
+    public void testingMethod2() {
+        System.out.println("method2 for testing");
+    }
 }
