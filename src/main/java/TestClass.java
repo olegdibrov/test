628690919c6128874b39aa531aacaffaf626f413
Oleh Dibrov <Oleh_Dibrov@epam.com>
2019-07-08 17:28:49 +0300

    Feature 1 in dev

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -7,4 +7,8 @@ public class TestClass {
     public void testingMethod2() {
         System.out.println("method2 for testing");
     }
+
+    public void feature1() {
+        System.out.println("feature1");
+    }
 }
